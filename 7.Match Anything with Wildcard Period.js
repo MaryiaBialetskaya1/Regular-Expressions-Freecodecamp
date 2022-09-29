48df6c802ff3652a36a76e36b8b797c974a86898
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-29 14:41:25 -0400

    add solution to task 7.Match Anything with Wildcard Period.js

---
+++ 7.Match Anything with Wildcard Period.js
@@ -0,0 +1,10 @@
+//7.Match Anything with Wildcard Period.js
+
+
+//The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words.
+
+//TASK: Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result = unRegex.test(exampleStr);
