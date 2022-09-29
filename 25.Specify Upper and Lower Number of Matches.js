75933bbdb0d76ec5993b81df92e2358d6606c53e
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-29 18:09:58 -0400

    add solution to task 26.Specify Only the Lower Number of Matches.js

--- 25.Specify Upper and Lower Number of Matches.js
+++ 25.Specify Upper and Lower Number of Matches.js
@@ -12,3 +12,5 @@ let ohStr = "Ohhh no";
 let ohRegex = /Oh{3,6}\sno/;
 let result = ohRegex.test(ohStr);
 
+//Remember to use \s after Oh{3,6} to include a white space, followed by no to pass all test cases. All test cases are written using a capital O, however the testcases could also be passed by using ignore-case : /oh{3,6}\sno/i
+
