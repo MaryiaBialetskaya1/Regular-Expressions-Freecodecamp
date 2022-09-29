e4a19b5d199f942d7163b57da60b212ee8e5c2c6
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-29 17:33:00 -0400

    edit solution 20.Match All Numbers.js

--- 20.Match All Numbers.js
+++ 20.Match All Numbers.js
@@ -4,6 +4,6 @@
 
 //TASK: Use the shorthand character class \d to count how many digits are in movie titles. Written out numbers ("six" instead of 6) do not count.
 
-et movieName = "2001: A Space Odyssey";
+let movieName = "2001: A Space Odyssey";
 let numRegex = /\d/g;
 let result = movieName.match(numRegex).length;
