c19a6b43f2861bb161df41ef2d7213e9e7861157
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-29 15:28:12 -0400

    add solution to task 18.Match All Letters and Numbers.js

---
+++ 18.Match All Letters and Numbers.js
@@ -0,0 +1,9 @@
+//18.Match All Letters and Numbers.js
+
+//The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]. This character class matches upper and lowercase letters plus numbers. Note, this character class also includes the underscore character (_).
+
+//TASK: Use the shorthand character class \w to count the number of alphanumeric characters in various quotes and strings.
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/gi; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
