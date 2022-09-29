d4c0cf0e3d91a9d51bc30e032fd211a01aee33c7
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-09-29 14:21:58 -0400

    add solution to task 3.Match a Literal String with Different Possibilities.js

---
+++ 3.Match a Literal String with Different Possibilities.js
@@ -0,0 +1,7 @@
+// 3.Match a Literal String with Different Possibilities.js
+
+// TASK: Complete the regex petRegex to match the pets dog, cat, bird, or fish.
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let result = petRegex.test(petString);
